98ec382a8b183a147d0558374bf13cfd3dcf71f4
Safik <safikwidiantoro@gmail.com>
2023-05-30 01:16:05 +0700

    Initial Commit

---
+++ ToRoo/ToRooApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToRooApp.swift
+//  ToRoo
+//
+//  Created by Safik Widiantoro on 30/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ToRooApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
