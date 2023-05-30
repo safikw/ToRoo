291db1dfbd9a0ddfa2f8c4050bf8f217ef3a51c7
amar <amaramzari92@gmai.com>
2023-05-30 12:11:45 +0700

    gw tadi ganteng banget cuy

---
+++ ToRoo/cacacac.swift
@@ -0,0 +1,20 @@
+//
+//  cacacac.swift
+//  ToRoo
+//
+//  Created by Amar Amzari on 30/05/23.
+//
+
+import SwiftUI
+
+struct cacacac: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct cacacac_Previews: PreviewProvider {
+    static var previews: some View {
+        cacacac()
+    }
+}
