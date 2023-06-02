4440509322683926e65d06e79eb5622d1624805b
Safik <safikwidiantoro@gmail.com>
2023-06-02 09:29:16 +0700

    "add func save sleepanalysis, add healthkit capabilities"

--- ToRoo/Model/SleepStore.swift
+++ ToRoo/Model/SleepStore.swift
@@ -19,17 +19,79 @@ struct SleepStore {
     }
     
     
+    func saveSleepAnalysis() {
+        guard let sleepType = HKObjectType.categoryType(forIdentifier: .sleepAnalysis) else {
+                return
+            }
+            
+        let inBedObject = HKCategorySample(type: sleepType, value: HKCategoryValueSleepAnalysis.InBed.rawValue, start: Date(), end: Date())
+        let asleepREMObject = HKCategorySample(type: sleepType, value: HKCategoryValueSleepAnalysis.asleepREM .rawValue, start: Date(), end: Date())
+            
+            let saveCompletionHandler: (Bool, Error?) -> Void = { success, error in
+                if let error = error {
+                    // Handle error
+                    print("Error: \(error.localizedDescription)")
+                } else if success {
+                    // Saved successfully
+                    print("Data saved in HealthKit")
+                } else {
+                    // Saving failed
+                    print("Failed to save data in HealthKit")
+                }
+            }
+            
+            healthStore?.save(inBedObject, withCompletion: saveCompletionHandler)
+            healthStore?.save(asleepREMObject, withCompletion: saveCompletionHandler)
+    }
+    
+    //Reading sleep Analysis Data
+    func retrieveSleepAnalysis() {
+        
+        // first, we define the object type we want
+        if let sleepType = HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis) {
+            
+            // Use a sortDescriptor to get the recent data first
+            let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+            
+            // we create our query with a block completion to execute
+            let query = HKSampleQuery(sampleType: sleepType, predicate: nil, limit: 30, sortDescriptors: [sortDescriptor]) { (query, tmpResult, error) -> Void in
+                
+                if error != nil {
+                    
+                    // something happened
+                    return
+                    
+                }
+                
+                if let result = tmpResult {
+                    
+                    // do something with my data
+                    for item in result {
+                        if let sample = item as? HKCategorySample {
+                            let value = (sample.value == HKCategoryValueSleepAnalysis.InBed.rawValue) ? "InBed" : "Asleep"
+                            print("Healthkit sleep: \(sample.startDate) \(sample.endDate) - value: \(value)")
+                        }
+                    }
+                }
+            }
+            
+            // finally, we execute our query
+            healthStore?.execute(query)
+        }
+    }
+    
+    
     func displaySleepStages() async{
         let sleepSampleType = HKCategoryType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!
         
         
         
-//        let stagePredicate = HKCategoryValueSleepAnalysis.predicateForSamples(.equalTo, value: .asleepREM)
-//
-//        let queryPredicate = HKSamplePredicate.sample(type: HKCategoryType(.sleepAnalysis), predicate: stagePredicate)
-//
-//        let sleepQuery = HKSampleQueryDescriptor(predicates: [queryPredicate], sortDescriptors: [])
-//
+        //        let stagePredicate = HKCategoryValueSleepAnalysis.predicateForSamples(.equalTo, value: .asleepREM)
+        //
+        //        let queryPredicate = HKSamplePredicate.sample(type: HKCategoryType(.sleepAnalysis), predicate: stagePredicate)
+        //
+        //        let sleepQuery = HKSampleQueryDescriptor(predicates: [queryPredicate], sortDescriptors: [])
+        //
     }
     
     
@@ -37,11 +99,11 @@ struct SleepStore {
         
         let readSleep = HKCategoryType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!
         let shareSleep = HKCategoryType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!
-//        let sleepCategory = HKCategoryValueSleepAnalysis.asleepDeep.rawValue
-//        let deepSleepSample  = HKCategorySample(type: sleepSampleType,
-//                                                value:sleepCategory,
-//                                                start: Date()-7,
-//                                                end: Date())
+        //        let sleepCategory = HKCategoryValueSleepAnalysis.asleepDeep.rawValue
+        //        let deepSleepSample  = HKCategorySample(type: sleepSampleType,
+        //                                                value:sleepCategory,
+        //                                                start: Date()-7,
+        //                                                end: Date())
         
         guard let healthStore = self.healthStore else {return completion(false)}
         
---
+++ ToRoo/Utils/SleepEnum.swift
@@ -0,0 +1,22 @@
+//
+//  SleepEnum.swift
+//  ToRoo
+//
+//  Created by Safik Widiantoro on 31/05/23.
+//
+
+import Foundation
+
+public enum HKCategoryValueSleepAnalysis : Int {
+    case InBed = 0
+    
+    case asleepUnspecified = 1
+    
+    case awake = 2
+    
+    case asleepCore = 3
+    
+    case asleepDeep = 4
+    
+    case asleepREM = 5
+}
