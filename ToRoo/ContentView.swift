898a85cd727243662a6c5f70b47e30af8401ddb4
Safik <safikwidiantoro@gmail.com>
2023-06-09 10:12:11 +0700

    uncomment

--- ToRoo/ContentView.swift
+++ ToRoo/ContentView.swift
@@ -13,10 +13,10 @@ struct ContentView: View {
     @StateObject var weekStore: WeekStore = WeekStore()
     
     var body: some View {
-        HomeView()
-//                InfiniteWeekView()
-//                    .environmentObject(weekStore)
-////                SleepSummaryView(healthStore: healthStore, weekStore: weekStore)
+//        HomeView()
+                InfiniteWeekView()
+                    .environmentObject(weekStore)
+                SleepSummaryView(healthStore: healthStore, weekStore: weekStore)
             
         
 
